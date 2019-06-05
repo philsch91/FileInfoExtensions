66a58ceec32651e00e4e17ea77f85e196c25d937
P. Schunker <philipp.schunker@allianz.at>
2019-06-05 18:16:37 +0200

    initial commit

---
+++ FileInfoExtensions/FileInfoExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FileExtensions {
+    public static class FileInfoExtensions {
+
+        public static bool IsLocked(this FileInfo fileInfo) {
+            FileStream stream = null;
+
+            try {
+                stream = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            } catch(IOException) {
+                //the file is unavailable because it is
+                //1. still being written to
+                //2. or being processed by another thread
+                //3. or does not exist (has already been processed)
+                return true;
+            } finally {
+                if(stream != null) {
+                    stream.Close();
+                }
+            }
+
+            //file is not locked
+            return false;
+        }
+    }
+}
